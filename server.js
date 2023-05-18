50da0ebc6688573d536f26099b0b332a9c8cabdc
Mohammad Hefny <mohammad.hefny@gmail.com>
2023-05-18 10:22:41 +0300

    gui: console display updates

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ function fn_startExpressServer ()
 function fn_displayHelp ()
 {
     console.log ("==============================================");
-    console.log (global.Colors.Bright + "Andruav Authentication Server version " +  JSON.stringify(v_pjson.version) + global.Colors.Reset);
+    console.log (global.Colors.Bright + "DE Authentication Server version " +  JSON.stringify(v_pjson.version) + global.Colors.Reset);
     console.log ("----------------------------------");
     console.log ("--config=config_filename config file ");
     console.log ("-h help ");
@@ -102,11 +102,11 @@ function fn_displayHelp ()
 function fn_displayInfo ()
 {
     console.log ("==============================================");
-    console.log (global.Colors.Bright + "Andruav Authentication Server version " +  JSON.stringify(v_pjson.version) + global.Colors.Reset);
+    console.log (global.Colors.Bright + "DE Authentication Server version " +  JSON.stringify(v_pjson.version) + global.Colors.Reset);
     console.log ("----------------------------------");
     console.log ("Server Name  " + global.Colors.BSuccess + global.m_serverconfig.m_configuration.server_id + global.Colors.Reset);
-    console.log ("listening on ip: " + global.Colors.BSuccess +  global.m_serverconfig.m_configuration.server_ip + " port: " + global.m_serverconfig.m_configuration.server_port + global.Colors.Reset);
-    console.log ("S2S WS ip: "  + global.Colors.BSuccess +  global.m_serverconfig.m_configuration.s2s_ws_listening_ip  + " port: " + global.m_serverconfig.m_configuration.s2s_ws_listening_port + global.Colors.Reset);
+    console.log ("listening on ip: " + global.Colors.BSuccess +  global.m_serverconfig.m_configuration.server_ip + global.Colors.Reset + " port: " + global.Colors.BSuccess + global.m_serverconfig.m_configuration.server_port + global.Colors.Reset);
+    console.log ("S2S WS ip: "  + global.Colors.BSuccess +  global.m_serverconfig.m_configuration.s2s_ws_listening_ip  + global.Colors.Reset + " port: " + global.Colors.BSuccess + global.m_serverconfig.m_configuration.s2s_ws_listening_port + global.Colors.Reset);
     
     if (global.m_serverconfig.m_configuration.enableLog!==true)
     {
