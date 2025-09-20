3dd813ff43828150fc3f10b7e5f92d2671e41c52
HefnySco <mohammad.hefny@gmail.com>
2025-09-20 08:39:42 +0300

    Feature: adding health path, Fix: error page

--- routes/js_router.js
+++ routes/js_router.js
@@ -1,33 +1,49 @@
-
 "use strict";
+const v_pjson = require('../package.json');
+let v_express = require('express');
+let v_router = v_express.Router();
 
-//var  v_favicon = require('express-favicon');
-var v_express = require('express');
-var v_router = v_express.Router();
-
-function fn_errorPage (v_res)
-{
+function fn_errorPage(v_res, isApiRequest = false) {
     v_res.status(404);
-    v_res.render('pages/404', { title: '404', message: 'This page does not exist.' });
+    if (isApiRequest) {
+        v_res.json({
+            [global.c_CONSTANTS.CONST_ERROR]: global.c_CONSTANTS.CONST_ERROR_NOT_FOUND,
+            [global.c_CONSTANTS.CONST_ERROR_MSG]: 'API endpoint not found',
+            [global.c_CONSTANTS.CONST_COMMAND]: 'error'
+        });
+    } else {
+        v_res.render('pages/404', { title: 'DE 404', message: 'This page does not exist.' });
+    }
 }
-
 
 function fn_create(app) {
-    app.use (v_router);
-    //app.use(favicon(__dirname + '/public/favicon.ico')); 
 
-   app.use('/public', v_express.static('public'));
+    const health_url = global.m_serverconfig.m_configuration.health_utl?global.m_serverconfig.m_configuration.health_utl:'/h';
+    
+    app.use(v_router);
 
-   app.use(global.c_CONSTANTS.CONST_WEB_FUNCTION,                  require('./js_router_web'));
-   app.use(global.c_CONSTANTS.CONST_AGENT_FUNCTION,                require('./js_router_agent'));
-   
+    // Health-check endpoint
+    app.get(`${health_url}/health`, (req, res) => {
+        res.status(200).json({
+            status: 'OK',
+            version: v_pjson.version,
+            server_id: global.m_serverconfig.m_configuration.server_id
+        });
+    });
 
-   // else is error
-   app.use((v_req, v_res, next) => {
-        fn_errorPage(v_res);
+    
+
+    app.use('/public', v_express.static('public'));
+
+    app.use(global.c_CONSTANTS.CONST_WEB_FUNCTION, require('./js_router_web'));
+    app.use(global.c_CONSTANTS.CONST_AGENT_FUNCTION, require('./js_router_agent'));
+
+    // Catch-all for unmatched routes
+    app.use((v_req, v_res, next) => {
+        const isApiRequest = v_req.originalUrl.startsWith(global.c_CONSTANTS.CONST_WEB_FUNCTION);
+        fn_errorPage(v_res, isApiRequest);
     });
 }
-
 
 module.exports = {
     fn_create: fn_create,
