851e2ccfa72e3dcd36927df895e71526d203c987
HefnySco <mohammad.hefny@gmail.com>
2026-06-23 00:19:53 +0300

    Fix dependencies and legacy issues
    - Align dependencies across both repos (mysql2, uuid, ws, helmet)
    - Upgrade helmet from v3 to v7 and fix API compatibility
    - Remove unused simple-json-db dependency
    - Create hlp_validation.js to eliminate String.prototype pollution
    - Update all prototype method calls to use validation functions
    - Remove duplicate 'use strict' declarations
    - Update node_modules via npm install

--- helpers/hlp_string.js
+++ helpers/hlp_string.js
@@ -1,71 +1,16 @@
 "use strict";
 var randomstring = require("randomstring");
+const v_validation = require("./hlp_validation");
 
-//http://stackoverflow.com/questions/1144783/replacing-all-occurrences-of-a-string-in-javascript
-String.prototype.replaceAll = function(search, replacement)
-{
-    var target = this;
-    return target.replace(new RegExp(search, 'g'), replacement);
-};
-
-String.prototype.fn_protectedFromInjection = function()
-{
-    var target = this;
-    return target.replace(new RegExp("--", 'g'), " ");
-};
-
-
-String.prototype.replacebyIndex = function (index,replaceBy)
-{
-    if (index == null) return null;
-    if (this.length < index) return null;
-    return this.substr(0, index) + replaceBy + s.substr(index + 1);
-};
-
-
-/**
- * contains letters or numbers
- */
-String.prototype.fn_isAlphanumeric = function ()
-{
-    return (this.match(/^[_a-z0-9]+$/i) != null);
-}
-
-
-/**
- * example: 5a.0.0
- */
-String.prototype.fn_isVersionFormat = function ()
-{
-    return (this.match(/([a-z0-9]+\.)([a-z0-9]+\.)([a-z0-9])+$/i) != null);
-}
-
-
-/**
- * example: This is a 5 Sample
- */
-String.prototype.fn_isAlphanumericSentence = function ()
-{
-    return (this.match(/^[A-Za-z0-9( )]+$/i) != null);
-}
-
-/**
- * contains both letters AND numbers
- */
-String.prototype.fn_isOnlyAlphanumeric = function ()
-{
-    return (this.match(/((^[0-9]+[a-z]+)|(^[a-z]+[0-9]+))+[0-9a-z]+$/i) != null);
-}
-
-
-/**
- * https://www.w3resource.com/javascript/form/email-validation.php
- */
-String.prototype.fn_isEmail = function ()
-{
-    return (this.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/i) != null);
-}
-
+// Re-export validation functions for backward compatibility
+exports.replaceAll = v_validation.replaceAll;
+exports.fn_protectedFromInjection = v_validation.protectedFromInjection;
+exports.replacebyIndex = v_validation.replaceByIndex;
+exports.fn_isAlphanumeric = v_validation.isAlphanumeric;
+exports.fn_isVersionFormat = v_validation.isVersionFormat;
+exports.fn_isAlphanumericSentence = v_validation.isAlphanumericSentence;
+exports.fn_isOnlyAlphanumeric = v_validation.isOnlyAlphanumeric;
+exports.fn_isEmail = v_validation.isEmail;
 
 exports.generateRandomString = function (strLen)
 {
@@ -78,9 +23,6 @@ exports.generateRandomString = function (strLen)
     return id;
 }
 
-
 exports.pad2 = function(number) {
-   
-    return (number < 10 ? '0' : '') + number
-  
+    return (number < 10 ? '0' : '') + number;
 }
